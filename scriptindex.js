ef12bdf98fa79e7c6beabaa1640cbfed7df9f786
Ambrizzz <5aariker@gmail.com>
2024-12-16 22:07:14 -0600

    Update scriptindex.js

--- scriptindex.js
+++ scriptindex.js
@@ -99,7 +99,7 @@
   ["Eagles", "Steelers", 27, 13, "Final"],
   ["Seahawks", "Packers", 13, 30, 'Final'],
   ["Vikings", "Bears", 27, 12, "Live", "Q4"],
-  ["Raiders", "Falcons", 3, 15, "Live", "Q4"],
+  ["Raiders", "Falcons", 9, 15, "Live", "Q4"],
   ];
 
   const week16Teams = [
